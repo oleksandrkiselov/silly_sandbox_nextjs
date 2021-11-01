3452550e36150315126452c1fd058e4020ac6a39
Oleksandr Kiselov <82498836+oleksandrkiselov@users.noreply.github.com>
2021-11-01 21:09:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,22 @@ import React from 'react'
 const Home = () => (
   <div>
 Home
+  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
+      <nav className="my-2 my-md-0 mr-md-3">
+        <Link href="/be">
+          <a className="p-2 text-dark">be</a>
+        </Link>
+        <Link href="/ua">
+          <a className="p-2 text-dark">ua</a>
+        </Link>
+        <Link href="/de">
+          <a className="p-2 text-dark">de</a>
+        </Link>
+        <Link href="/br">
+          <a className="p-2 text-dark">br</a>
+        </Link>
+      </nav>
+    </div>
   </div>
 )
 
